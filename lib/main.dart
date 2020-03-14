12a5d0d96c661716d2ed99080f83e03275861671
Bozz <p.srinikorn@gmail>
2020-03-14 13:25:38 +0700

    [Add] math in pubspec and calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,205 +1,147 @@
-//import 'package:flutter/material.dart';
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: ThemeData(
-//        // This is the theme of your application.
-//        //
-//        // Try running your application with "flutter run". You'll see the
-//        // application has a blue toolbar. Then, without quitting the app, try
-//        // changing the primarySwatch below to Colors.green and then invoke
-//        // "hot reload" (press "r" in the console where you ran "flutter run",
-//        // or simply save your changes to "hot reload" in a Flutter IDE).
-//        // Notice that the counter didn't reset back to zero; the application
-//        // is not restarted.
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page X'),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  // This widget is the home page of your application. It is stateful, meaning
-//  // that it has a State object (defined below) that contains fields that affect
-//  // how it looks.
-//
-//  // This class is the configuration for the state. It holds the values (in this
-//  // case the title) provided by the parent (in this case the App widget) and
-//  // used by the build method of the State. Fields in a Widget subclass are
-//  // always marked "final".
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//    setState(() {
-//      // This call to setState tells the Flutter framework that something has
-//      // changed in this State, which causes it to rerun the build method below
-//      // so that the display can reflect the updated values. If we changed
-//      // _counter without calling setState(), then the build method would not be
-//      // called again, and so nothing would appear to happen.
-//      _counter++;
-//    });
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    // This method is rerun every time setState is called, for instance as done
-//    // by the _incrementCounter method above.
-//    //
-//    // The Flutter framework has been optimized to make rerunning build methods
-//    // fast, so that you can just rebuild anything that needs updating rather
-//    // than having to individually change instances of widgets.
-//    return Scaffold(
-//      appBar: AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: Column(
-//          // Column is also a layout widget. It takes a list of children and
-//          // arranges them vertically. By default, it sizes itself to fit its
-//          // children horizontally, and tries to be as tall as its parent.
-//          //
-//          // Invoke "debug painting" (press "p" in the console, choose the
-//          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//          // to see the wireframe for each widget.
-//          //
-//          // Column has various properties to control how it sizes itself and
-//          // how it positions its children. Here we use mainAxisAlignment to
-//          // center the children vertically; the main axis here is the vertical
-//          // axis because Columns are vertical (the cross axis would be
-//          // horizontal).
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'Hello x x x ',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
-//}
-
-// new
-
 import 'package:flutter/material.dart';
-
-void main() => runApp((MaterialApp(
-  title: 'MyApp',
-  home: Scaffold(
-    appBar:AppBar(
-      title: Text('my app'),
+import 'package:math_expressions/math_expressions.dart';
+void main() => runApp(
+  MaterialApp(
+    title: 'My App',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: MyApp(),
     ),
-    body : MyApp(),
-  )
-)
-),
+  ),
 );
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
   String result = '0';
+  String eval = '0';
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-          alignment: Alignment.centerRight,
-          child: Text(result, style: TextStyle(),),
-    ),
-    Column(
-      children: <Widget>[
-        Row(
+        Expanded(
+          child: Container(
+            alignment: Alignment.centerRight,
+            child: Text(
+              result,
+              style: TextStyle(fontSize: 36),
+            ),
+          ),
+        ),
+        Column(
           children: <Widget>[
-            _buildkeyPadButton(title:'1'),
-            _buildKeyPadButton(
-              title: '2',
+            Row(
+              children: <Widget>[
+                _buildKeyPadButton(title: '1'),
+                _buildKeyPadButton(
+                  title: '2',
+                ),
+                _buildKeyPadButton(
+                  title: '3',
+                ),
+                _buildKeyPadButton(
+                  title: '+',
+                ),
+              ],
             ),
-            _buildKeyPadButton(
-              title:'3',
+            Row(
+              children: <Widget>[
+                _buildKeyPadButton(
+                  title: '4',
+                ),
+                _buildKeyPadButton(
+                  title: '5',
+                ),
+                _buildKeyPadButton(
+                  title: '6',
+                ),
+                _buildKeyPadButton(
+                  title: '-',
+                ),
+              ],
             ),
-            _buildKeyPadButton(
-              title:'+',
-            )
-          ]
-        )
-      ]
-        )
-      ]FlatButton _buildKeyPadButton({String title}) =>
-      FlatButton(child: Text(title),
-          onPressed: (){
-          setState :(){
-          result = result + title;
-        })
+            Row(
+              children: <Widget>[
+                _buildKeyPadButton(
+                  title: '7',
+                ),
+                _buildKeyPadButton(
+                  title: '8',
+                ),
+                _buildKeyPadButton(
+                  title: '9',
+                ),
+                _buildKeyPadButton(
+                  title: 'x',
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                _buildKeyPadButton(
+                  title: '0',
+                ),
+                _buildKeyPadButton(
+                  title: 'C',
+                ),
+                _buildKeyPadButton(
+                  title: '%',
+                ),
+                _buildKeyPadButton(
+                  title: '=',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
-}
-
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-      Container(
-        alignment: Alignment.centerRight,
-        child: Text('0'),
-    ),
-      Column(
-        children: <Widget>[
-      Row(
-          children : <Widget>[
-            _buildKeyPadButton(),
-          FlatButton(child:,Text('1'),),
-          FlatButton(child:,Text('2'),),
-          FlatButton(child:,Text('3'),),
-          FlatButton(child:,Text('+'),),
-    ],
-    ),
-    )
-    ],
+  Expanded _buildKeyPadButton({String title}) {
+    return Expanded(
+      child: FlatButton(
+        color: Colors.redAccent,
+        padding: EdgeInsets.all(20),
+        child: Text(
+          title,
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          _addKey(title);
+        },
+      ),
     );
-    )
   }
-  FlatButton _buildKeyPadButton({String title}) =>
-      FlatButton(child: Text(title),
-          onPressed: (){
-          setState :(){
-          result = result + title;
-        })
-});
+  void _addKey(String symbol) {
+    setState(() {
+      if (symbol == '=') {
+        Parser p = Parser();
+        Expression exp = p.parse(eval);
+        ContextModel cm = ContextModel();
+        double tmp = exp.evaluate(EvaluationType.REAL, cm);
+        result = '$tmp';
+        eval = '$tmp';
+      } else if (symbol == 'C') {
+        result = '0';
+        eval = '0';
+      } else {
+        if (result == '0') {
+          eval = symbol;
+          result = symbol;
+        } else {
+          if (symbol == 'x') {
+            eval += '*';
+          } else if (symbol == '%') {
+            eval += '/';
+          } else {
+            eval += symbol;
+          }
+          result += symbol;
+        }
+      }
+    });
+  }
 }
